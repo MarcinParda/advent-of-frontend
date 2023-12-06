566cc675e4d413916e9a834934eaf8bdb11cd440
MarcinParda <marcin98parda@gmail.com>
2023-12-07 00:28:43 +0100

    refactor: use my own event emitter instead of using node:events

--- tasks/2023-12-05/index.ts
+++ tasks/2023-12-05/index.ts
@@ -1,4 +1,39 @@
-import EventEmitter from 'node:events';
+type EventListener = (...args: unknown[]) => void;
+
+class EventEmitter {
+  private listeners: Map<string, EventListener[]>;
+
+  constructor() {
+    this.listeners = new Map();
+  }
+
+  on(event: string, listener: EventListener): void {
+    const eventListeners = this.listeners.get(event) || [];
+    eventListeners.push(listener);
+    this.listeners.set(event, eventListeners);
+  }
+
+  emit(event: string, ...args: unknown[]): void {
+    const eventListeners = this.listeners.get(event);
+    if (eventListeners) {
+      eventListeners.forEach((listener) => {
+        listener(...args);
+      });
+    }
+  }
+
+  off(event: string, listener: EventListener): void {
+    const eventListeners = this.listeners.get(event);
+    if (eventListeners) {
+      const filteredListeners = eventListeners.filter((l) => l !== listener);
+      if (filteredListeners.length === 0) {
+        this.listeners.delete(event);
+      } else {
+        this.listeners.set(event, filteredListeners);
+      }
+    }
+  }
+}
 
 export class ChristmasEmitter {
   #eventEmitter = new EventEmitter();
